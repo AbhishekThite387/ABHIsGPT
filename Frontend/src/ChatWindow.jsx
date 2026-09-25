dcefacd82f372acef8158d34513bcd3022155d0b
AbhishekThite <thiteabhi443@gmail.com>
2026-09-25 17:20:57 +0530

    Added Dropdown options

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -7,6 +7,7 @@ import {ScaleLoader} from "react-spinners";
 function ChatWindow () {
     const{prompt , setPrompt , reply , setReply , currThreadId, prevChats , setPrevChats ,setNewChat} = useContext(MyContext);
     const[loading , setLoading] = useState(false);
+    const[isOpen , setIsOpen] = useState(true);
 
     const getReply = async () => {
         setLoading(true);
@@ -60,6 +61,14 @@ function ChatWindow () {
                     <span className="userIcon"><i className="fa-solid fa-user"></i></span>
                 </div>
             </div>
+            {
+                isOpen &&
+                <div className="dropDown">
+                    <div className="dropDownItem"><i class="fa-solid fa-cloud-arrow-up"></i>Upgrade plan</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-gear"></i>Settings</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log out</div>
+                </div>
+            }
 
             <Chat></Chat>
             <ScaleLoader color="#fff" loading={loading}>
